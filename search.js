ecc058f95fe2db92e6fb1ff832fd7ca618a8d39b
Santiago Donoso <sand@kea.dk>
2021-10-27 15:23:59 +0200

    ...

---
+++ search.js
@@ -0,0 +1,35 @@
+function show_results(){
+  document.querySelector("#results").style.display = "grid"
+
+}
+
+function hide_results(){
+  document.querySelector("#results").style.display = "none"
+}
+
+async function search(){
+  console.log("searching...")
+  document.querySelector("#results").innerHTML = ""
+  // const search = event.target.value
+  console.log(search)
+  // Connect to the API
+
+  setTimeout( ()=>{
+    const search = document.querySelector("#search_for").value
+    const conn = await fetch(`https://coderspage.com/iceland/search-items?search=${search}`)
+    const data = await conn.json()
+    data.forEach( item => {
+      let div_item = `<div class="result">${item.item_name}</div>`
+      document.querySelector("#results").insertAdjacentHTML("afterbegin", div_item)
+    })
+  }, 2000 )
+
+
+  // https://coderspage.com/iceland/search-items?search
+  // Get the items
+  // Show the items in the drop down
+}
+
+
+
+
